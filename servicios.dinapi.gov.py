2aea9cfc71b63cc214524dafe9f2936702699395
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2020-11-08 10:53:07 +0300

    Create servicios.dinapi.gov.py

---
+++ servicios.dinapi.gov.py
@@ -0,0 +1,111 @@
+.gov government
+
+.py Paraguay
+
+Organization: Secretaria Nacional de Tecnologias de la Información y Comunicaciones
+
+İndustrıal Property Informatıon Consultancy Service
+
+https://servicios.dinapi.gov.py/agente/procesar_busqueda_mov_expediente_v2.php?id=1575881
+
+back-end DBMS: PostgreSQL
+
+available databases [1]:
+[+] public
+
+Database: public
+[92 tables]
++-----------------------------+
+| agente                      |
+| aviso_agente                |
+| bancard                     |
+| biometrico_checkinout       |
+| biometrico_userinfo         |
+| busqueda_resultado          |
+| clasificador_niza           |
+| control_ora                 |
+| control_pg                  |
+| datos_claudia_comp          |
+| denominacion                |
+| dependencia                 |
+| directorio_internos_ip      |
+| diseno                      |
+| documento                   |
+| feriados                    |
+| form001_reg_marca           |
+| form001_titulares           |
+| form002_excepcion           |
+| form002_ren_marca           |
+| form002_titulares           |
+| form006_agente              |
+| form006_documentos          |
+| form006_registro_poder      |
+| form006_solicitante         |
+| form007_documentos          |
+| form007_pub_diarios         |
+| form008_documentos          |
+| form008_solicitante         |
+| form008_solicitud_oposicion |
+| form016_documento           |
+| form016_solicitante         |
+| form_datos_facturacion      |
+| form_logo                   |
+| form_marca                  |
+| form_orden_publicacion      |
+| form_redpi                  |
+| form_titular                |
+| lista                       |
+| lista_opcion                |
+| log_agente                  |
+| log_conexion_agente         |
+| log_conexion_usuario        |
+| log_logotipo_clasific       |
+| log_marca                   |
+| log_usuario_agentes         |
+| logotipo                    |
+| logotipo_clasific           |
+| marca                       |
+| marca_cambio                |
+| marca_cambio_det            |
+| marca_producto              |
+| marca_titular               |
+| mark_sign                   |
+| mensaje                     |
+| movimiento                  |
+| movimiento_diseno           |
+| movimiento_docum            |
+| movimiento_patente          |
+| pagos                       |
+| pais                        |
+| patente                     |
+| patente_inventor            |
+| patente_resumen             |
+| patente_sip                 |
+| patente_titular             |
+| payment_response            |
+| pdf_notificacion_agente     |
+| pdf_notificacion_agente_arc |
+| pdf_patente                 |
+| pdf_poder                   |
+| poder                       |
+| recibo                      |
+| recibo_tasa_docum           |
+| recibo_tasa_marca           |
+| redpi_procesos              |
+| revistas                    |
+| revistas_clasificados       |
+| status                      |
+| tipo_documento              |
+| tipo_movimiento             |
+| tipo_tasa                   |
+| tipo_tramite                |
+| titular                     |
+| usuario_agentes             |
+| usuario_marcas              |
+| usuarios                    |
+| v_secuencia                 |
+| viena_clase                 |
+| viena_elemento              |
+| viena_grupo                 |
+| web_control_migracion       |
++-----------------------------+
