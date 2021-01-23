7b4791fcd1dab30352ec87630f765460bb613d87
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2021-01-23 18:51:53 +0300

    Create stat.gov.mk

---
+++ stat.gov.mk
@@ -0,0 +1,33 @@
+.gov Government
+
+.mk Macedonia
+
+Macedonia Northern Republic
+
+State Statistical Office
+
+State Institute of Statistics
+
+http://www.stat.gov.mk/OblastOpsto_en.aspx?id=13
+
+the back-end DBMS: Microsoft SQL Server 2008
+
+available databases [18]:                                                                                                                                            
+[*] ANKETI
+[*] GK
+[*] KORISNICI_APLIKACII_VNES
+[*] master
+[*] MobilnaApp
+[*] model
+[*] msdb
+[*] PXWeb_2
+[*] pxweb_proba
+[*] PXWEB_SQL
+[*] ReportServer
+[*] ReportServerTempDB
+[*] tempdb
+[*] TransMonee
+[*] WEB_APLIKACII
+[*] WEB_NOVA
+[*] WEBR0411
+[*] WEBtestInj
