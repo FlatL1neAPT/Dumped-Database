e3336917b260f4341c29e1400a37a9ea1612eadd
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2021-04-04 16:43:42 +0300

    Create jmcapiata.gov.py

---
+++ jmcapiata.gov.py
@@ -0,0 +1,38 @@
+gov Government
+
+.py Paraguay
+
+Municipal Board of the City of Capiata
+
+Municipal Board of Capiata
+
+http://www.jmcapiata.gov.py/noticias_mas.php?id=1024
+
+the back-end DBMS is MySQL
+web application technology: Apache
+the back-end DBMS: MySQL >= 5.0.0
+
+available databases [1]:
+[+] jmcapiat_web
+
+Database: jmcapiat_web
+[17 tables]
++----------------------+
+| concejales_directiva |
+| concejales_listado   |
+| consejales           |
+| expresidentes        |
+| galeria_1            |
+| galeria_2            |
+| hist_1               |
+| hist_2               |
+| historia             |
+| nomina               |
+| noticias             |
+| noticias_galeria     |
+| ordenanzas           |
+| resoluciones         |
+| varios               |
+| varios_cat           |
+| videos               |
++----------------------+
