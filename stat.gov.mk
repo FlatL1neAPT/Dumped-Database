b9af0c9c3a5304df9e0498428b4d095d10353c96
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2021-04-04 18:21:49 +0300

    Update stat.gov.mk

--- stat.gov.mk
+++ stat.gov.mk
@@ -10,6 +10,9 @@ State Institute of Statistics
 
 http://www.stat.gov.mk/OblastOpsto_en.aspx?id=13
 
+the back-end DBMS is Microsoft SQL Server
+web server operating system: Windows 2012 R2 or 8.1
+web application technology: ASP.NET 4.0.30319, ASP.NET, Microsoft IIS 8.5
 the back-end DBMS: Microsoft SQL Server 2008
 
 available databases [18]:                                                                                                                                            
