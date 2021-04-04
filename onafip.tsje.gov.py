a3114282ff47a0407a9870396949693ce36873a9
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2021-04-04 18:08:21 +0300

    Create onafip.tsje.gov.py

---
+++ onafip.tsje.gov.py
@@ -0,0 +1,76 @@
+.gov Government
+
+.py Paraguay
+
+Onafıp - National Political Finance Observatory
+
+https://onafip.tsje.gov.py/subtitulo.php?publicacion_id=210
+
+the back-end DBMS is PostgreSQL
+web application technology: Apache 2.4.38, PHP 5.6.40
+the back-end DBMS: PostgreSQL
+
+available databases [3]:
+[+] information_schema
+[+] pg_catalog
+[+] public
+
+Database: pg_catalog
+[55 tables]
++-------------------------+
+| pg_aggregate            |
+| pg_am                   |
+| pg_amop                 |
+| pg_amproc               |
+| pg_attrdef              |
+| pg_attribute            |
+| pg_auth_members         |
+| pg_authid               |
+| pg_cast                 |
+| pg_class                |
+| pg_collation            |
+| pg_constraint           |
+| pg_conversion           |
+| pg_database             |
+| pg_db_role_setting      |
+| pg_default_acl          |
+| pg_depend               |
+| pg_description          |
+| pg_enum                 |
+| pg_event_trigger        |
+| pg_extension            |
+| pg_foreign_data_wrapper |
+| pg_foreign_server       |
+| pg_foreign_table        |
+| pg_index                |
+| pg_inherits             |
+| pg_init_privs           |
+| pg_language             |
+| pg_largeobject          |
+| pg_largeobject_metadata |
+| pg_namespace            |
+| pg_opclass              |
+| pg_operator             |
+| pg_opfamily             |
+| pg_pltemplate           |
+| pg_policy               |
+| pg_proc                 |
+| pg_range                |
+| pg_replication_origin   |
+| pg_rewrite              |
+| pg_seclabel             |
+| pg_shdepend             |
+| pg_shdescription        |
+| pg_shseclabel           |
+| pg_statistic            |
+| pg_tablespace           |
+| pg_transform            |
+| pg_trigger              |
+| pg_ts_config            |
+| pg_ts_config_map        |
+| pg_ts_dict              |
+| pg_ts_parser            |
+| pg_ts_template          |
+| pg_type                 |
+| pg_user_mapping         |
++-------------------------+
